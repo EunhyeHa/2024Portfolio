2d068f7ebc185e91cb0aa382e01c830e40971747
EunhyeHa <grace9695@naver.com>
2024-06-10 17:12:38 +0900

    240610 about gsap 추가

--- about.js
+++ about.js
@@ -1,56 +1,48 @@
-// const txtWord = document.querySelector('.txt-word');
-// const txtWordPs = txtWord.querySelectorAll('p');
-
-// window.addEventListener('scroll', function() {
-//     const introTxtWrapper = document.querySelector('.intro-txt-wrapper');
-//     const txtWordRect = txtWord.getBoundingClientRect();
-    
-//     if (txtWordRect.top <= introTxtWrapper.offsetHeight) {
-//         txtWordPs.forEach((p, index) => {
-//             gsap.to(p, { opacity: 1, y: 0, delay: index * 0.2 });
-//         });
-//     }
-// });
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   const txtWord = document.querySelector('.txt-word');
-//   const txtWordPs = txtWord.querySelectorAll('p');
-
-//   const observer = new IntersectionObserver(entries => {
-//       entries.forEach(entry => {
-//           if (entry.isIntersecting) {
-//               txtWordPs.forEach((p, index) => {
-//                   setTimeout(() => {
-//                       p.style.opacity = 1;
-//                       p.style.transform = 'translateY(0)';
-//                   }, index * 200);
-//               });
-//           }
-//       });
-//   }, { threshold: 1 });
-
-//   observer.observe(txtWord);
-// });
-
-
 // intro-scollEvent
+
 document.addEventListener('DOMContentLoaded', function() {
   gsap.registerPlugin(ScrollTrigger);
 
-  const txtWordPs = document.querySelectorAll('.txt-word p');
+  const images = [
+      './assets/images/about/1.jpg',
+      './assets/images/about/2.jpg',
+      './assets/images/about/3.jpg',
+      './assets/images/about/4.jpg',
+      './assets/images/about/5.jpg',
+      './assets/images/about/6.jpg',
+      './assets/images/about/7.jpg',
+      './assets/images/about/8.jpg',
+      './assets/images/about/9.jpg',
+      './assets/images/about/10.jpg',
+  ];
 
-  txtWordPs.forEach((p, index) => {
-      gsap.fromTo(p, 
-          { opacity: 0, y: 20 }, 
-          { opacity: 1, y: 0, 
-            scrollTrigger: {
-                trigger: p,
-                start: 'top 50%',
-                end: 'top 60%',
-                scrub: true,
-                markers: true // 디버깅을 위해 스크롤 트리거 마커를 표시합니다.
-            }
-          });
-  });
+  images.forEach((image, index) => {
+    gsap.to('#txt-img', {
+        backgroundImage: `url(${image})`,
+        scrollTrigger: {
+            trigger: '.about-intro',
+            start: () => `top top+=${index * 100}vh`,
+            end: () => `top top+=${(index + 1) * 100}vh`,
+            scrub: true,
+            markers: true,
+            toggleActions: 'play none none reverse'
+        }
+    });
+});
+
+const txtWordPs = document.querySelectorAll('.txt-word p');
+txtWordPs.forEach((p, index) => {
+    gsap.fromTo(p, 
+        { opacity: 0, y: 20 }, 
+        { opacity: 1, y: 0, 
+          scrollTrigger: {
+              trigger: '.about-intro',
+              start: () => `top top+=${index * 100}vh`,
+              end: () => `top top+=${(index + 1) * 100}vh`,
+              scrub: true,
+              markers: false,
+              toggleActions: 'play none none reverse'
+          }
+        });
+});
 });
