8b676b911d685c856885ee6c4b44f187702beb44
EunhyeHa <grace9695@naver.com>
2024-07-01 09:35:04 +0900

    about 수정, favicon 추가, logo 추가

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+## 240613 목 ##
+- connet 프로젝트 방향성 확립
+- connet UIUX 디자인 시작 및 포트폴리오 구성내용 변경
+
 ## 240611 화 ##
 - about 페이지 수정 : career mouse hover 이모지 추가, skills&tools 수정
 - index 페이지 js 세부 조정
--- about.js
+++ about.js
@@ -1,3 +1,20 @@
+// title-imgSlide
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
 // intro-scollEvent
 document.addEventListener('DOMContentLoaded', function () {
     gsap.registerPlugin(ScrollTrigger);
@@ -26,22 +43,22 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // const txtWordPs = document.querySelectorAll('.txt-word p');
-    // txtWordPs.forEach((p, index) => {
-    //     gsap.fromTo(p,
-    //         { opacity: 0, y: 20 },
-    //         {
-    //             opacity: 1, y: 0,
-    //             scrollTrigger: {
-    //                 trigger: '.about-intro',
-    //                 start: () => `top top+=${index * 100}vh`,
-    //                 end: () => `top top+=${(index + 1) * 100}vh`,
-    //                 scrub: true,
-    //                 markers: false,
-    //                 toggleActions: 'play none none reverse'
-    //             }
-    //         });
-    // });
+    const txtWordPs = document.querySelectorAll('.txt-word p');
+    txtWordPs.forEach((p, index) => {
+        gsap.fromTo(p,
+            { opacity: 0, y: 20 },
+            {
+                opacity: 1, y: 0,
+                scrollTrigger: {
+                    trigger: '.about-intro',
+                    start: () => `top top+=${index * 100}vh`,
+                    end: () => `top top+=${(index + 1) * 100}vh`,
+                    scrub: true,
+                    markers: false,
+                    toggleActions: 'play none none reverse'
+                }
+            });
+    });
 
 });
 
