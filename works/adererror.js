8e6ab9fd55f4307db3b4523f4286c093ab1db3d0
EunhyeHe <grace9695@naver.com>
2024-10-28 13:33:48 +0900

    241028 CONNET 상세페이지 디테일 수정

--- works/adererror.js
+++ works/adererror.js
@@ -5,11 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                // 요소가 뷰포트에 보이면 3초 후 애니메이션을 시작
                 setTimeout(() => {
                     entry.target.classList.add('animate'); // 애니메이션 클래스 추가
-                }, 500);
-                observer.unobserve(entry.target); // 애니메이션 실행 후 관찰 중지
+                }, 300);
+                observer.unobserve(entry.target);
             }
         });
     }, {
